61b27c6122adb072e858ac2daaa0ed3790d465c4
ravee2001 <s6406021620076@email.kmutnb.ac.th>
2024-12-03 11:28:48 +0700

    update  Ex3JS

--- Lecture-02/EXCERCISE/Ex-03.js
+++ Lecture-02/EXCERCISE/Ex-03.js
@@ -3,7 +3,7 @@ let b = 2;
 let c = 4;
 let d = 7;
 let equation1 = eval("a + a") ; 
-let equation2 = eval("7*4-2");
+let equation2 = eval("d*c-b");
 let equation3 = eval("a + a + a + a + a") ; 
 
 // eval() จะคำนวณ "a + a"
