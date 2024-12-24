66c706b7fbfdd8847ee3202880ca6747624069b9
ravee2001 <s6406021620076@email.kmutnb.ac.th>
2024-12-24 09:42:31 +0700

    a

--- Lecture-03/Exercism/apple.js
+++ Lecture-03/Exercism/apple.js
@@ -2,10 +2,3 @@
 // let values1 = ['Fries', 2, true];
 // let values1 = ['Mars', 9, 'Apple';
 
-const values1 = {
-    name: "Pipey",       // พร็อพเพอร์ตี้ name มีค่าเริ่มต้นเป็น "Pipey"
-    age: 8,              // พร็อพเพอร์ตี้ age มีค่าเริ่มต้นเป็น 8
-    whatName() {         // ฟังก์ชัน whatName
-        return this.name; // คืนค่าพร็อพเพอร์ตี้ name ของออบเจ็กต์นี้
-    },
-};
